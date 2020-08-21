aab94fd30b5f6b82effe16ae692cfce8f3f7d1ee
Matt Pilott <matt@creativelittledots.co.uk>
2020-08-21 18:19:42 +0100

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>About</title>
+	{@html metas}
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+<script>
+	let metas = '<meta name="description" content="Two: A test of svelte head with @html">';
+</script>
